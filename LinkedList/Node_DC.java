d1a879468aed4d635a9ed20f2dec5f946a727276
David Chen <davidc3287@gmail.com>
2020-12-01 16:41:46 -0500

    add node class

--- LinkedList/Node_DC.java
+++ LinkedList/Node_DC.java
@@ -8,6 +8,23 @@ public class Node_DC {
     }
 
     public Node_DC(String value, Node_DC nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+
+    public Node_DC getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Node_DC getNext() {
+        return this.nextNode;
+    }
+
+    public void setNext(Node_DC nextNode) {
         this.nextNode = nextNode;
     }
 }
