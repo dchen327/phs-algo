ad91f379dd7b284adbabd0983fb52aea71f275a2
David Chen <davidc3287@gmail.com>
2020-12-18 16:00:22 -0500

    add folder and setup

---
+++ percolation/Percolation_DC.java
@@ -0,0 +1,25 @@
+
+/**
+ * Percolation. Given a composite system comprised of randomly distributed insulating
+ *  and metallic materials: what fraction of the materials need to be metallic so
+ *  that the composite system is an electrical conductor? Given a porous landscape 
+ * with water on the surface (or oil below), under what conditions will the water 
+ * be able to drain through to the bottom (or the oil to gush through to the surface)? 
+ * Scientists have defined an abstract process known as percolation to model such situations.
+ * 
+ * @David Chen
+ * @Java 1.8.0 - 12/18/20
+ * 
+ * Run with `java-algs4 Percolation_DC.java && javac-algs4 Percolation_DC.java`
+ * after installing the stuff from algs4.jar
+ */
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation_DC {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
