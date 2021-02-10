6c4f561f7ce69ae07e44aa6725fb9e43f9ddd16e
David Chen <davidc3287@gmail.com>
2021-02-10 11:17:33 -0500

    initial commit

---
+++ TelephoneSearch/Telephone.java
@@ -0,0 +1,14 @@
+/**
+ * Write a telephone lookup program, Telephone.java. Read the data set of 1,000 names and telephone numbers from the attached file.
+ * It contains the numbers in random order. Handle lookups by name and also reverse lookups by phone number. 
+ * Use Comparator and a binary search for both lookups.
+ * 
+ * @David Chen
+ * @Java 1.8.0 - 2/10/21
+ */
+
+public class Telephone {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
