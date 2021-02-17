e43a6cbf46d1575283104ccff79064aae8e9a43e
David Chen <davidc3287@gmail.com>
2021-02-17 14:04:59 -0500

    fix file stuff

--- Autocomplete/Autocomplete.java
+++ Autocomplete/Autocomplete.java
@@ -10,11 +10,8 @@
  * @David Chen
  * @Java 1.8.0 - 2/17/21
  */
-
-import java.util.Arrays;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-import java.util.Comparator;
+import java.io.*;
+import java.util.*;
 
 public class Autocomplete {
     private Term[] terms;
@@ -64,27 +61,28 @@ public class Autocomplete {
         return lastIndex - firstIndex + 1;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // read in the terms from a file
         String filename = args[0];
-        In in = new In(filename);
-        int N = in.readInt();
+        Scanner fileScan = new Scanner(new File(filename));
+        int N = fileScan.nextInt();
         Term[] terms = new Term[N];
         for (int i = 0; i < N; i++) {
-            double weight = in.readDouble(); // read the next weight
-            in.readChar(); // scan past the tab
-            String query = in.readLine(); // read the next query
+            double weight = fileScan.nextDouble(); // read the next weight
+            String query = fileScan.next(); // read the next query
             terms[i] = new Term(query, weight); // construct the term
         }
+        System.out.println(terms[10]);
 
         // read in queries from standard input and print out the top k matching terms
         int k = Integer.parseInt(args[1]);
-        Autocomplete autocomplete = new Autocomplete(terms);
-        while (StdIn.hasNextLine()) {
-            String prefix = StdIn.readLine();
-            Term[] results = autocomplete.allMatches(prefix);
-            for (int i = 0; i < Math.min(k, results.length); i++)
-                StdOut.println(results[i]);
-        }
+        // Autocomplete autocomplete = new Autocomplete(terms);
+        // Scanner scan = new Scanner(System.in);
+        // while (scan.hasNextLine()) {
+        //     String prefix = scan.readLine();
+        //     Term[] results = autocomplete.allMatches(prefix);
+        //     for (int i = 0; i < Math.min(k, results.length); i++)
+        //         System.out.println(results[i]);
+        // }
     }
 }
