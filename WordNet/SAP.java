be39a95096779e31939b7ffa5c2232691de72063
David Chen <davidc3287@gmail.com>
2021-06-14 14:46:10 -0400

    add testing files

--- WordNet/SAP.java
+++ WordNet/SAP.java
@@ -15,6 +15,8 @@ import java.util.HashSet;
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.DirectedCycle;
 
 public class SAP {
@@ -101,6 +103,15 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args) {
-        System.out.println("hello world");
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 }
